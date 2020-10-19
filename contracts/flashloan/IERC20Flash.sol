1e70190f7a60277d66b7325c1efc444ab9fc7c97
snaketh4xor <snaketh4xor@protonmail.com>
2020-10-19 18:16:20 +0530

    missed to delete IERC20Flash

--- contracts/flashloan/IERC20Flash.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.8;
-
-interface IERC20Flash {
-    function executeOnERC20FlashLoan(address token, uint256 amount, uint256 debt, bytes calldata params) external;
-}
