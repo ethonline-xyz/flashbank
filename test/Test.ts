69ffeca3fd9deb4a98114c0ca93b4a7eb0827b61
snaketh4xor <snaketh4xor@protonmail.com>
2020-10-20 15:55:04 +0530

    Boom Prettier again,added sample testcases

--- test/Test.ts
+++ test/Test.ts
@@ -5,4 +5,21 @@ import {deployContract, solidity} from "ethereum-waffle";
 chai.use(solidity);
 const {expect} = chai;
 
-describe("", () => {});
+describe("", () => {
+  beforeEach(async () => {});
+
+  describe("Testing Flashloan", async () => {
+    it("Testing ERC20 flashloan", async () => {});
+    it("Testing ETH flashloan", async () => {});
+  });
+
+  describe("Testing Deposits", async () => {
+    it("Testing ERC20 deposit", async () => {});
+    it("Testing ETH deposit", async () => {});
+  });
+
+  describe("Testing Withdraws", async () => {
+    it("Testing ERC20 withdraw", async () => {});
+    it("Testing ETH withdraw", async () => {});
+  });
+});
